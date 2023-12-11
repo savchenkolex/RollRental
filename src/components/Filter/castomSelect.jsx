3aaf32c48c94a9be8eca5a1e6f97c311476a1051
lex <savchenko.lex@gmail.com>
2023-12-11 23:35:31 +0200

    minor changes

--- src/components/Filter/castomSelect.jsx
+++ src/components/Filter/castomSelect.jsx
@@ -1,38 +1,39 @@
-import { useState } from 'react';
-import css from './Filter.module.css';
-import { useField } from 'formik';
+import { useState } from "react";
+import css from "./Filter.module.css";
+import { useField } from "formik";
 
-export default function CustomSelect ({label, options, ...props}) {
-    const [field, meta] = useField(props);
-    const [isOpen, setIsOpen] = useState(false);
+export default function CustomSelect({ label, options, ...props }) {
+  const [field, meta] = useField(props);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleOption = (value) => {
-        field.onChange({ target: { name: props.name, value } });
-        setIsOpen(false);
-    }
+  const handleOption = (value) => {
+    field.onChange({ target: { name: props.name, value } });
+    setIsOpen(false);
+  };
 
-
-    return (<>
-        <div className="custom-select-container">
-      <label>{label}</label>
-      <div
-        className={`custom-select ${isOpen ? 'open' : ''}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="selected-option">{field.value}</div>
-        {isOpen && (
-          <ul className="options-list">
-            {options.map((option) => (
-              <li key={option} onClick={() => handleOptionClick(option)}>
-                {option}
-              </li>
-            ))}
-          </ul>
-        )}
+  return (
+    <>
+      <div className={css.customSelectContainer}>
+        <label>{label}</label>
+        <div
+          className={`custom-select ${isOpen ? "open" : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="selected-option">{field.value}</div>
+          {isOpen && (
+            <ul className="options-list">
+              {options.map((option) => (
+                <li key={option} onClick={() => handleOption(option)}>
+                  {option}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {meta.touched && meta.error ? (
+          <div className="error">{meta.error}</div>
+        ) : null}
       </div>
-      {meta.touched && meta.error ? <div className="error">{meta.error}</div> : null}
-    </div>
-    </>);
-
+    </>
+  );
 }
-
